796fdb260cd52e3225906e4c1359f805dd8f1bb4
Wesley T. Honeycutt <wessubba8888@yahoo.com>
2019-06-27 13:34:12 -0500

    initial commit, migration from LunAero, lro first column must be current frame, contour only output quack

---
+++ LunCV/__init__.py
@@ -0,0 +1,8 @@
+#!/bin/usr/python3 -B
+# -*- coding: utf-8 -*-
+
+from __main__ import *
+from .LCVconfig import USEGUI
+
+if USEGUI:
+	from .Debuggui import Gui
---
+++ __init__.py
@@ -0,0 +1,2 @@
+#!/bin/usr/python3 -B
+# -*- coding: utf-8 -*-
